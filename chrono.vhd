df08893e297c3d01302844ac251c27dd7cf3b32c
Dlanyar <dlnr@protonmail.com>
2025-03-25 11:54:01 +0100

    Syntaxe changes

--- chrono.vhd
+++ chrono.vhd
@@ -1,6 +1,6 @@
 library ieee; 
 use ieee.std_logic_1164.all; 
-use ieee.numeric_std.all
+use ieee.numeric_std.all;
 
 ENTITY CHRONOMETRE  IS 
 
@@ -8,13 +8,13 @@ ENTITY CHRONOMETRE  IS
 
   CE_1s : IN std_logic; 
   CLK : IN std_logic; 
-  WAIT_t IN std_logic; 
-  START IN std_logic; 
+  WAIT_t : IN std_logic; 
+  START : IN std_logic; 
   RESET : IN std_logic; 
   sec_unit : OUT std_logic_vector(3 downto 0);
   sec_dec : OUT std_logic_vector(3 downto 0); 
   min_unit : OUT std_logic_vector(3 downto 0); 
-  min_dec : OUT std_logic_vector(3 downto 0); 
+  min_dec : OUT std_logic_vector(3 downto 0)
   ); 
 END CHRONOMETRE; 
 
@@ -28,7 +28,7 @@ port (
 CLK : in std_logic;
 R : in std_logic;
 D : in std_logic; 
-Q : out std_logic;
+Q : out std_logic
 );
 end component BASCULE_CHRONO;
 
@@ -38,7 +38,7 @@ CLK : in std_logic;
 R : in std_logic;
 CE : in std_logic; 
 TC : out std_logic;
-S :out std_logic_vector(3 downto 0); 
+S :out std_logic_vector(3 downto 0)
 );
 end component CPT_SU_CHRONO;
 
@@ -49,7 +49,7 @@ CLK : in std_logic;
 R : in std_logic;
 CE : in std_logic; 
 TC : out std_logic;
-S :out std_logic_vector(3 downto 0); 
+S :out std_logic_vector(3 downto 0)
 );
 end component CPT_SD_CHRONO;
 
@@ -60,7 +60,7 @@ CLK : in std_logic;
 R : in std_logic;
 CE : in std_logic; 
 TC : out std_logic;
-S :out std_logic_vector(3 downto 0); 
+S :out std_logic_vector(3 downto 0) 
 );
 end component CPT_MU_CHRONO;
 
@@ -70,7 +70,7 @@ CLK : in std_logic;
 R : in std_logic;
 CE : in std_logic; 
 TC : out std_logic;
-S :out std_logic_vector(3 downto 0); 
+S :out std_logic_vector(3 downto 0)
 );
 end component CPT_MD_CHRONO;
 
@@ -79,6 +79,8 @@ component COMP_CHRONO
 port (
 in1 : in std_logic_vector(3 downto 0);
 in2 : in std_logic_vector(3 downto 0);
-S : out std_logic; 
+S : out std_logic
 );
 end component COMP_CHRONO;
+
+end behavior;
--- display.vhd
+++ display.vhd
@@ -1,6 +1,6 @@
 library ieee; 
 use ieee.std_logic_1164.all; 
-use ieee.numeric_std.all
+use ieee.numeric_std.all;
 
 ENTITY DISPLAY  IS 
 
@@ -8,34 +8,34 @@ ENTITY DISPLAY  IS
 
   CE_1ms : IN std_logic; 
   CE_1s : IN std_logic; 
-  CLK IN std_logic; 
-  data_disp1_L0 IN std_logic_vector(3 downto 0);
-  data_disp1_L1 IN std_logic_vector(3 downto 0);
-  data_disp1_R0 IN std_logic_vector(3 downto 0);
-  data_disp1_R1 IN std_logic_vector(3 downto 0);
-  data_disp2_L0 IN std_logic_vector(3 downto 0);
-  data_disp2_L1 IN std_logic_vector(3 downto 0);
-  data_disp2_R0 IN std_logic_vector(3 downto 0);
-  data_disp2_R1 IN std_logic_vector(3 downto 0);
+  CLK : IN std_logic; 
+  data_disp1_L0 : IN std_logic_vector(3 downto 0);
+  data_disp1_L1 : IN std_logic_vector(3 downto 0);
+  data_disp1_R0 : IN std_logic_vector(3 downto 0);
+  data_disp1_R1 : IN std_logic_vector(3 downto 0);
+  data_disp2_L0 : IN std_logic_vector(3 downto 0);
+  data_disp2_L1 : IN std_logic_vector(3 downto 0);
+  data_disp2_R0 : IN std_logic_vector(3 downto 0);
+  data_disp2_R1 : IN std_logic_vector(3 downto 0);
   AN : OUT std_logic_vector(7 downto 0);
-  LEDS : OUT std_logic_vector(7 downto 0);
+  LEDS : OUT std_logic_vector(7 downto 0)
   ); 
 END DISPLAY; 
 
 architecture behavior of DISPLAY is 
 
-signal : tregister_sig, mux_1b_sig std_logic; 
-signal : mux_4b_sig std_logic_vector(3 downto 0 ); 
-signal : out_counter_3b_sig std_logic_vector( 2 downto 0 ); 
-signal : out_trans_3v8_sig std_logic_vector(7 downto 0); 
-signal : out_trans_7seg_sig std_logic_vector(6 downto 0); 
+signal tregister_sig, mux_1b_sig : std_logic; 
+signal mux_4b_sig : std_logic_vector(3 downto 0 ); 
+signal out_counter_3b_sig : std_logic_vector( 2 downto 0 ); 
+signal out_trans_3v8_sig : std_logic_vector(7 downto 0); 
+signal out_trans_7seg_sig : std_logic_vector(6 downto 0); 
 
 
 component counter_3b_E
 port (
 CLK : in std_logic;
 CE : in std_logic;
-O : out std_logic_vector(3 downto 0);
+O : out std_logic_vector(3 downto 0)
 );
 end component counter_3b_E;
 
@@ -43,7 +43,7 @@ component Tregister_1b
 port (
 CLK : in std_logic;
 T : in std_logic;
-O : out std_logic;
+O : out std_logic
 );
 end component Tregister_1b;
 
@@ -51,7 +51,7 @@ component register_8b1
 port (
 CLK : in std_logic;
 D : in std_logic_vector(7 downto 0);
-O : out std_logic_vector(7 downto 0);
+O : out std_logic_vector(7 downto 0)
 );
 end component register_8b1;
 
@@ -59,7 +59,7 @@ component register_8b2
 port (
 CLK : in std_logic;
 D : in std_logic_vector(7 downto 0);
-O : out std_logic_vector(7 downto 0);
+O : out std_logic_vector(7 downto 0)
 );
 end component register_8b2;
 
@@ -76,7 +76,7 @@ G : in std_logic_vector(3 downto 0);
 H : in std_logic_vector(3 downto 0); 
 S : in std_logic_vector(2 downto 0); 
 
-O : out std_logic_vector(3 downto 0); 
+O : out std_logic_vector(3 downto 0)
 );
 end component mux_8x1x4b;
 
@@ -92,7 +92,7 @@ F : in std_logic;
 G : in std_logic;  
 H : in std_logic; 
 sel : in std_logic_vector(2 downto 0); 
-O : out std_logic; 
+O : out std_logic 
 
 
 );
@@ -101,7 +101,7 @@ end component mux_8x1x1b;
 component transcoder_3v8 
 port (
 A : in std_logic_vector(2 downto 0); 
-O : out std_logic_vector(7 downto 0); 
+O : out std_logic_vector(7 downto 0)
 );
 end component transcoder_3v8;
 
@@ -109,7 +109,7 @@ end component transcoder_3v8;
 component transcoder_7seg
 port (
 A : in std_logic_vector(3 downto 0); 
-O : out std_logic_vector(6 downto 0); 
+O : out std_logic_vector(6 downto 0) 
 
 );
 end component transcoder_7seg;
@@ -145,14 +145,14 @@ U3 : Tregister_1b
 
 U4 : mux_8x1x1b
   port map(
-    A => 1,  
-    B => 1, 
+    A => '1',  
+    B => '1', 
     C => tregister_sig, 
-    D => 1, 
-    E =>1, 
-    F => 1, 
-    G =>1, 
-    H=>1, 
+    D => '1', 
+    E =>'1', 
+    F => '1', 
+    G =>'1', 
+    H=>'1', 
     sel => out_counter_3b_sig, 
     O => mux_1b_sig
   ); 
@@ -180,7 +180,7 @@ U6 : transcoder_7seg
 U7 : register_8b2
   port map(
     CLK => CLK, 
-    D => out_trans_7seg_sig & mux_1b_sig
+    D => out_trans_7seg_sig & mux_1b_sig,
     O => LEDS
   );
 
