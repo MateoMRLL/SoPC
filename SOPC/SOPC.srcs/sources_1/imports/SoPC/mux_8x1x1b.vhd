069b3fa2920f81935e75de4006140545d54d8035
Dlanyar <dlnr@protonmail.com>
2025-03-26 13:39:42 +0100

    Initialized Vivado project

---
+++ SOPC/SOPC.srcs/sources_1/imports/SoPC/mux_8x1x1b.vhd
@@ -0,0 +1,22 @@
+ -- Fichier : mux_8x1x4b.vhd
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity mux_8x1x1b is
+    Port ( A, B, C, D, E, F, G, H : in  STD_LOGIC;
+           sel : in  STD_LOGIC_VECTOR(2 downto 0);
+           O : out STD_LOGIC);
+end mux_8x1x1b;
+
+architecture Behavioral of mux_8x1x1b is
+begin
+        -- Utilisation de when else pour la sélection de l'entrée en fonction de S
+        O <= A when sel = "000" else
+             B when sel = "001" else
+             C when sel = "010" else
+             D when sel = "011" else
+             E when sel = "100" else
+             F when sel = "101" else
+             G when sel = "110" else
+             H;  -- S = "111" et donc H est choisi par défaut
+end Behavioral;
