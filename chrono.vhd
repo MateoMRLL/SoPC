485f265e6cb4289a42467b846849368540acb75f
Dlanyar <dlnr@protonmail.com>
2025-04-01 10:02:57 +0200

    Chronometre Correct

--- chrono.vhd
+++ chrono.vhd
@@ -1,86 +1,143 @@
-library ieee; 
-use ieee.std_logic_1164.all; 
+library ieee;
+use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-ENTITY CHRONOMETRE  IS 
-
+ENTITY CHRONOMETRE IS
   PORT(
+    CE_1s   : IN std_logic;
+    CLK     : IN std_logic;
+    WAIT_t  : IN std_logic;
+    START   : IN std_logic;
+    RESET   : IN std_logic;
+    sec_unit : OUT std_logic_vector(3 downto 0);
+    sec_dec  : OUT std_logic_vector(3 downto 0);
+    min_unit : OUT std_logic_vector(3 downto 0);
+    min_dec  : OUT std_logic_vector(3 downto 0)
+  );
+END CHRONOMETRE;
 
-  CE_1s : IN std_logic; 
-  CLK : IN std_logic; 
-  WAIT_t : IN std_logic; 
-  START : IN std_logic; 
-  RESET : IN std_logic; 
-  sec_unit : OUT std_logic_vector(3 downto 0);
-  sec_dec : OUT std_logic_vector(3 downto 0); 
-  min_unit : OUT std_logic_vector(3 downto 0); 
-  min_dec : OUT std_logic_vector(3 downto 0)
-  ); 
-END CHRONOMETRE; 
+architecture behavior of CHRONOMETRE is
 
-architecture behavior of CHRONOMETRE is 
+  signal out_comp, out_bascule, in_d_bascule, ce_sec_unit, ce_sec_dec, ce_min_unit, ce_min_dec : std_logic;
+  signal min_unit_int, min_dec_int : std_logic_vector(3 downto 0);
 
-signal out_comp, out_bascule, in_d_bascule, ce_sec_unit, ce_sec_dec , ce_min_unit, ce_min_unit: std_logic; 
-signal min_unit_int, min_dec_int : std_logic_vector(3 downto 0); 
+  component BASCULE_CHRONO
+  port (
+    CLK : in std_logic;
+    R   : in std_logic;
+    D   : in std_logic; 
+    Q   : out std_logic
+  );
+  end component;
 
-component BASCULE_CHRONO
-port (
-CLK : in std_logic;
-R : in std_logic;
-D : in std_logic; 
-Q : out std_logic
-);
-end component BASCULE_CHRONO;
+  component CPT_SU_CHRONO
+  port (
+    CLK : in std_logic;
+    R   : in std_logic;
+    CE  : in std_logic; 
+    TC  : out std_logic;
+    S   : out std_logic_vector(3 downto 0)
+  );
+  end component;
 
-component CPT_SU_CHRONO
-port (
-CLK : in std_logic;
-R : in std_logic;
-CE : in std_logic; 
-TC : out std_logic;
-S :out std_logic_vector(3 downto 0)
-);
-end component CPT_SU_CHRONO;
+  component CPT_SD_CHRONO
+  port (
+    CLK : in std_logic;
+    R   : in std_logic;
+    CE  : in std_logic; 
+    TC  : out std_logic;
+    S   : out std_logic_vector(3 downto 0)
+  );
+  end component;
 
+  component CPT_MU_CHRONO
+  port (
+    CLK : in std_logic;
+    R   : in std_logic;
+    CE  : in std_logic; 
+    TC  : out std_logic;
+    S   : out std_logic_vector(3 downto 0)
+  );
+  end component;
 
-component CPT_SD_CHRONO
-port (
-CLK : in std_logic;
-R : in std_logic;
-CE : in std_logic; 
-TC : out std_logic;
-S :out std_logic_vector(3 downto 0)
-);
-end component CPT_SD_CHRONO;
+  component CPT_MD_CHRONO
+  port (
+    CLK : in std_logic;
+    R   : in std_logic;
+    CE  : in std_logic; 
+    TC  : out std_logic;
+    S   : out std_logic_vector(3 downto 0)
+  );
+  end component;
 
+  component COMP_CHRONO
+  port (
+    in1 : in std_logic_vector(3 downto 0);
+    in2 : in std_logic_vector(3 downto 0);
+    S   : out std_logic
+  );
+  end component;
 
-component CPT_MU_CHRONO
-port (
-CLK : in std_logic;
-R : in std_logic;
-CE : in std_logic; 
-TC : out std_logic;
-S :out std_logic_vector(3 downto 0) 
-);
-end component CPT_MU_CHRONO;
+begin
+  -- Flip-Flop for START signal
+  B1 : BASCULE_CHRONO
+    port map (
+      CLK => CLK,
+      R   => RESET,
+      D   => START,
+      Q   => out_bascule
+    );
 
-component CPT_MD_CHRONO
-port (
-CLK : in std_logic;
-R : in std_logic;
-CE : in std_logic; 
-TC : out std_logic;
-S :out std_logic_vector(3 downto 0)
-);
-end component CPT_MD_CHRONO;
+  -- Comparator
+  COMP1 : COMP_CHRONO
+    port map (
+      in1 => min_unit_int,
+      in2 => "0000",
+      S   => out_comp
+    );
 
+  -- Seconds unit counter
+  C1 : CPT_SU_CHRONO
+    port map (
+      CLK => CLK,
+      R   => RESET,
+      CE  => CE_1s,
+      TC  => ce_sec_dec,
+      S   => sec_unit
+    );
 
-component COMP_CHRONO
-port (
-in1 : in std_logic_vector(3 downto 0);
-in2 : in std_logic_vector(3 downto 0);
-S : out std_logic
-);
-end component COMP_CHRONO;
+  -- Seconds tens counter
+  C2 : CPT_SD_CHRONO
+    port map (
+      CLK => CLK,
+      R   => RESET,
+      CE  => ce_sec_dec,
+      TC  => ce_min_unit,
+      S   => sec_dec
+    );
+
+  -- Minutes unit counter
+  C3 : CPT_MU_CHRONO
+    port map (
+      CLK => CLK,
+      R   => RESET,
+      CE  => ce_min_unit,
+      TC  => ce_min_dec,
+      S   => min_unit_int
+    );
+
+  -- Minutes tens counter
+  C4 : CPT_MD_CHRONO
+    port map (
+      CLK => CLK,
+      R   => RESET,
+      CE  => ce_min_dec,
+      TC  => open,
+      S   => min_dec_int
+    );
+
+  -- Output assignments
+  min_unit <= min_unit_int;
+  min_dec  <= min_dec_int;
 
 end behavior;
