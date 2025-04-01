9c91fe7903b059a6495443e862d3f412e81a09b9
Dlanyar <dlnr@protonmail.com>
2025-04-01 10:42:54 +0200

    normally end need to check chrono.vhd

--- COMP_CHRONO.vhd
+++ COMP_CHRONO.vhd
@@ -4,19 +4,14 @@ use IEEE.STD_LOGIC_ARITH.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 entity COMP_CHRONO is
-  port (
-    in1 : in std_logic_vector(3 downto 0);
-    in2 : in std_logic_vector(3 downto 0);
-    S   : out std_logic
-  );
+    port (
+        in1 : in std_logic_vector(3 downto 0);
+        in2 : in std_logic_vector(3 downto 0);
+        S   : out std_logic
+    );
 end entity COMP_CHRONO;
 
-architecture Behavioral of COMP_CHRONO is 
-  signal result : std_logic : '0'; 
-begin 
-  if in1 = "0101" and in2 = "0100" then 
-    S <= '1'; 
-  else 
-  S <= '0'; 
-  end if; 
-end architecture Behavioral; 
+architecture Behavioral of COMP_CHRONO is
+begin
+    S <= '1' when in1 = "0101" and in2 = "0100" else '0';
+end architecture Behavioral;
